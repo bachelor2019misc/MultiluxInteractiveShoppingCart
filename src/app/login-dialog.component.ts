a069894786d82c70b73ec4703ad820c148fca987
Kristin Hagen <kristin.haa@hotmail.com>
2019-02-13 15:37:43 +0100

    #14FinishLoginDialogComponent

--- src/app/login-dialog.component.ts
+++ src/app/login-dialog.component.ts
@@ -5,14 +5,19 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
     template: `
         <form [formGroup]="form" (ngSubmit)="submit(form)">
-            <h1 mat-dialog-title>Add file</h1>
+            <h1 mat-dialog-title>Log in</h1>
             <mat-dialog-content>
                 <mat-form-field>
-                    <input matInput formControlName="filename" placeholder="Enter filename">
-                </mat-form-field>
+                <input matInput formControlName="username" placeholder="Enter username" (ngOnInit)="username(form)">
+                    </mat-form-field>
+            </mat-dialog-content>
+            <mat-dialog-content>
+            <mat-form-field>
+            <input matInput formControlName="password" placeholder="Enter password" (ngOnInit)="password(form)">
+            </mat-form-field>
             </mat-dialog-content>
             <mat-dialog-actions>
-                <button mat-button type="submit">Add</button>
+                <button mat-button type="submit">Log in</button>
                 <button mat-button type="button" (click)="dialogRef.close()">Cancel</button>
             </mat-dialog-actions>
         </form>
@@ -30,11 +35,13 @@ export class LoginDialogComponent implements OnInit {
 
     ngOnInit() {
         this.form = this.formBuilder.group({
-          filename: this.data.filename ? this.data.filename : ''
+          username: this.data.username ? this.data.username : '',
+          password : this.data.password ? this.data.password : ''
         })
     }
     
     submit(form) {
-        this.dialogRef.close(`${form.value.filename}`);
+        this.dialogRef.close(`${form.value.username.password}`);
     }
+    
 }
