72a90ce06a9aa4590965c6fca5cd7014149efe87
Unknown <einar_weltan@hotmail.com>
2019-02-14 22:06:46 +0100

    #5 Add modal inside product-selection for sub-products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,6 @@ export class AppComponent {
   openAddFileDialog(file?) {
     this.fileNameDialogRef = this.dialog.open(LoginDialogComponent, {
       data: {
-        filename: file ? file.name : ''
       }
     });
 
